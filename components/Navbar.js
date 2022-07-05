948ab09e4b7cbabd2702e847226964d7d93d6977
Akhil Waghmare <akhilwaghmare@gmail.com>
2022-07-05 07:52:44 -0700

    Updated dependencies and homepage text

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,60 +1,72 @@
+/** @jsxRuntime classic */
 /** @jsx jsx */
-import { css, jsx } from '@emotion/core';
+import { css, jsx } from "@emotion/core";
 
-import Link from 'next/link';
-import logo from '../assets/img/Logo.svg';
-import dribbble from '../assets/img/dribbble.svg';
-import github from '../assets/img/github.svg';
-import linkedin from '../assets/img/linkedin.svg';
-import medium from '../assets/img/medium.svg';
-import { queries } from '../assets/styles/theme';
+import Link from "next/link";
+import logo from "../assets/img/Logo.svg";
+import dribbble from "../assets/img/dribbble.svg";
+import github from "../assets/img/github.svg";
+import linkedin from "../assets/img/linkedin.svg";
+import medium from "../assets/img/medium.svg";
+import { queries } from "../assets/styles/theme";
 
 const navbarStyle = css`
-    margin: 0 auto;
-    padding-top: 48px;
+  margin: 0 auto;
+  padding-top: 48px;
 
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
 
-    .logo {
-        height: 48px;
+  .logo {
+    height: 48px;
+  }
+
+  .social-icons {
+    @media screen and (max-width: ${queries.small}px) {
+      display: none;
     }
-
-    .social-icons {
-        @media screen and (max-width: ${queries.small}px) {
-            display: none;
-        }
-    }
+  }
 `;
 
 export function Navbar() {
-    return (
-        <div css={navbarStyle}>
-            <img className='logo' src={logo} />
-            <div className='social-icons'><SocialRow /></div>
-        </div>
-    )
+  return (
+    <div css={navbarStyle}>
+      <img className="logo" src={logo} />
+      <div className="social-icons">
+        <SocialRow />
+      </div>
+    </div>
+  );
 }
 
 const socialRowStyle = css`
-    display: grid;
-    grid-template-columns: repeat(4,1fr);
-    grid-gap: 20px;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 20px;
 
-    a, img {
-        height: 24px;
-    }
+  a,
+  img {
+    height: 24px;
+  }
 `;
 
 export function SocialRow() {
-    return (
-        <div css={socialRowStyle}>
-            <a href='https://dribbble.com/awdesigns' target='_blank'><img src={dribbble} /></a>
-            <a href='https://github.com/akhilwaghmare' target='_blank'><img src={github} /></a>
-            <a href='https://www.linkedin.com/in/akhilwaghmare' target='_blank'><img src={linkedin} /></a>
-            <a href='https://medium.com/@akhilwaghmare' target='_blank'><img src={medium} /></a>
-        </div>
-    );
+  return (
+    <div css={socialRowStyle}>
+      <a href="https://dribbble.com/awdesigns" target="_blank">
+        <img src={dribbble} />
+      </a>
+      <a href="https://github.com/akhilwaghmare" target="_blank">
+        <img src={github} />
+      </a>
+      <a href="https://www.linkedin.com/in/akhilwaghmare" target="_blank">
+        <img src={linkedin} />
+      </a>
+      <a href="https://medium.com/@akhilwaghmare" target="_blank">
+        <img src={medium} />
+      </a>
+    </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,77 @@
+/** @jsxRuntime classic */
 /** @jsx jsx */
-import { css, jsx } from '@emotion/core';
+import { css, jsx } from "@emotion/core";
 
 import { SocialRow } from "../components/Navbar";
-import Base from '../layouts/Base';
-import { colors, queries } from '../assets/styles/theme';
+import Base from "../layouts/Base";
+import { colors, queries } from "../assets/styles/theme";
 
 const indexStyle = css`
-    margin-top: 72px;
+  margin-top: 72px;
 
-    .text-content {
-        a {
-            color: ${colors.white};
-            text-decoration: none;
-        }
-    
-        a:hover {
-            color: ${colors.primary};
-            border-bottom: 2px solid currentColor;
-        }
+  .text-content {
+    a {
+      color: ${colors.white};
+      text-decoration: none;
     }
 
-    .social-icons {
-        width: 156px;
-        margin-bottom: 24px;
-        display: none;
-        
-        @media screen and (max-width: ${queries.small}px) {
-            display: block;
-        }
+    a:hover {
+      color: ${colors.primary};
+      border-bottom: 2px solid currentColor;
     }
+  }
+
+  .social-icons {
+    width: 156px;
+    margin-bottom: 24px;
+    display: none;
+
+    @media screen and (max-width: ${queries.small}px) {
+      display: block;
+    }
+  }
 `;
 
 function Index() {
+  let content = (
+    <div css={indexStyle}>
+      <div className="text-content">
+        <h1>Hi, I'm Akhil.</h1>
+        <h2>Technologist aspiring to build great products</h2>
+        <h3>
+          Associate Consultant @{" "}
+          <a href="https://bain.com" target="_blank">
+            Bain &amp; Company
+          </a>
+        </h3>
+        <h3>
+          Former iOS intern @{" "}
+          <a href="https://squareup.com/us/en" target="_blank">
+            Square
+          </a>
+          ,{" "}
+          <a href="https://www.washingtonpost.com/" target="_blank">
+            The Washington Post
+          </a>
+          , and{" "}
+          <a href="https://www.glooko.com" target="_blank">
+            Glooko
+          </a>
+        </h3>
+        <h3>
+          Former Design Chair @{" "}
+          <a href="https://www.thecrimson.com" target="_blank">
+            The Harvard Crimson
+          </a>
+        </h3>
+      </div>
+      <div className="social-icons">
+        <SocialRow />
+      </div>
+    </div>
+  );
 
-    let content = (
-        <div css={indexStyle}>
-            <div className='text-content'>
-                <h1>Hi, I'm Akhil.</h1>
-                <h2>Product designer + iOS developer</h2>
-                <h3>Former Design Chair @ <a href='https://www.thecrimson.com' target='_blank'>The Harvard Crimson</a></h3>
-                <h3>Former iOS intern @ <a href='https://squareup.com/us/en' target='_blank'>Square</a>, <a href='https://www.washingtonpost.com/' target='_blank'>The Washington Post</a>, and <a href='https://www.glooko.com' target='_blank'>Glooko</a></h3>
-            </div>
-            <div className='social-icons'><SocialRow /></div>
-        </div>
-    );
-
-    return (<Base>{content}</Base>)
+  return <Base>{content}</Base>;
 }
 
 export default Index;
